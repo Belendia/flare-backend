5901c8914cce7dd36504b20010996b6be44b2475
belendia <belendia@gmail.com>
2020-04-21 09:08:34 +0300

    Change the string message with response code 422 to dictionary be easily shown in the frontend

--- app/api.py
+++ app/api.py
@@ -1,5 +1,7 @@
 from flask_appbuilder import ModelRestApi
 from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder.models.filters import BaseFilter
+from sqlalchemy import or_
 
 from . import appbuilder, db
 from .models import Language
@@ -12,6 +14,11 @@ class LanguageModelApi(ModelRestApi):
     add_columns = ['name', 'code']
     edit_columns = ['name', 'code']
     list_columns = ['name', 'code']
+
+    def response_422(self, message):
+        m = message.split(": ")
+
+        return  self.response(422, message={m[1].replace("language.",""):m[0]})
 
 appbuilder.add_api(LanguageModelApi)
 
